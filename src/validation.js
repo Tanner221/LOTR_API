613a9ba3201f940dc7cd99f62bba303c5adf16f1
Tanner Robinson <Rob16041@byui.edu>
2023-03-07 08:59:04 -0700

    Adding validation

--- src/validation.js
+++ src/validation.js
@@ -1,5 +1,11 @@
 const { check } = require('express-validator');
 
 exports.CheckLocationValidation = [
-  check('name', 'Name of Location is Required').not().isEmpty()
+  check('name', 'Name of the location is required').escape().trim().not().isEmpty(),
+  check('coordinates', 'Coordinates of the location is requried').escape().trim().not().isEmpty(),
+  check('description', 'Description of the location is required').escape().trim().not().isEmpty(),
+  check('image', 'Image path of the location is requried').escape().trim().not().isEmpty(),
+  check('poplulation', 'The population of an area must be included').escape().trim().not().isEmpty(),
+  check('areaType', 'The area type is required').escape().trim().not().isEmpty(),
+  check('hospitalityLevel', 'How hospitibal is the area? Level 1-5').escape().trim().not().isEmpty()
 ]
