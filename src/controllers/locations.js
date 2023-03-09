86f940ab0dfceaa6db4fcd842d6db888d9d1c503
Tanner Robinson <Rob16041@byui.edu>
2023-03-09 09:06:46 -0700

    Location routes controller completed

--- src/controllers/locations.js
+++ src/controllers/locations.js
@@ -1,7 +1,53 @@
+const { BSONTypeError } = require('bson');
 const { validationResult } = require('express-validator');
+const { ObjectId } = require('mongodb');
 const mongodb = require('../db/connect');
 
 let controller = {}
+
+controller.getAllLocations = async (req, res) => {
+  const result = await mongodb.getDb().db('LOTR').collection('Locations').find();
+  result.toArray().then(
+    (items) => {
+      if(items.length > 0){
+        res.status(200).json(items)
+      } else {
+        res.status(404).json({
+            message: "No locations found"
+          });
+      }
+    }
+  )
+}
+
+controller.getOneLocation = async (req, res) => {
+  let id;
+  try{
+    id = new ObjectId(req.params.id);
+  }
+  catch(e){
+    if(e instanceof BSONTypeError){
+      res.status(400).json({
+        message: 'Bad id type'
+      })
+    }
+  }
+  const result = await mongodb.getDb().db('LOTR').collection('Locations').find({_id: id});
+  result.toArray()
+  .then(
+    (items) => {
+      if(items.length > 0){
+        res.status(200).json(items[0]);
+      }
+      else{
+        res.status(404).json({
+          message: 'Error: No location found'
+        })
+      }
+    }
+  )
+
+}
 
 controller.addLocation = async (req, res) => {
   //check for errors
@@ -16,7 +62,7 @@ controller.addLocation = async (req, res) => {
     coordinates: req.body.coordinates,
     description: req.body.description,
     image: req.body.image,
-    poplulation: req.body.poplulation,
+    population: req.body.poplulation,
     areaType: req.body.areaType,
     hospitalityLevel: req.body.hospitalityLevel
   }
--- src/routes/locations.js
+++ src/routes/locations.js
@@ -2,6 +2,8 @@ const routes = require('express').Router();
 const validator = require('../validation');
 const locationsController = require('../controllers/locations')
 
+routes.get('/', locationsController.getAllLocations)
+routes.get('/:id', locationsController.getOneLocation)
 routes.post('/', validator.CheckLocationValidation,locationsController.addLocation)
 
 module.exports = routes
